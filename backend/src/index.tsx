7552ecc38af13f9429c0962d934803eaf72fdf6e
Amanrathi07 <amanrathi4321@gmail.com>
2026-03-07 00:10:19 +0530

    bun jsx

--- backend/src/index.tsx
+++ backend/src/index.tsx
@@ -5,11 +5,28 @@ const app = new Hono();
 
 
 
+app.use(logger())
+
+const View = () => {
+  return (
+    <html>
+      <body>
+        <h1>Hello Hono!</h1>
+      </body>
+    </html>
+  )
+}
+
+
 app.get("/", (c) => {
     return c.json({
         message:"working"
     })
 });
+
+app.get('/page', (c) => {
+  return c.html(<View />)
+})
 
 export default app;
 
