77f0cab04e8f6e12a084f888bff529e986a1944b
Amanrathi07 <amanrathi4321@gmail.com>
2026-03-15 23:51:58 +0530

    mas break

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,15 @@
 import  express  from "express"
 import dotenv from "dotenv"
-
+import cors from "cors"
 import authRoute from "./routes/auth.route";
 const app = express() ;
 
-app.use(express.json())
 
+app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:5173", 
+    credentials: true
+}))
 dotenv.config()
 
 app.get("/",(req,res)=>{
