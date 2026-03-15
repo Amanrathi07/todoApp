9d6ca0ce35cc5c4adc667c5fcd5a7ea1710ff94d
Amanrathi07 <amanrathi4321@gmail.com>
2026-03-15 21:59:55 +0530

    next day

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,17 @@
 import  express  from "express"
+import dotenv from "dotenv"
 
 const app = express() ;
 
+app.use(express.json())
+
+dotenv.config()
 
 app.get("/",(req,res)=>{
     res.send("working")
 })
 
 
-app.listen(3000)
+app.listen(3000,()=>{
+    console.log("server is running on http://localhost:3000/")
+})
