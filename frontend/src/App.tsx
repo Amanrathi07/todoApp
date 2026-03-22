19aedbc2d64714db534f639bce4359690ac68a02
Amanrathi07 <amanrathi4321@gmail.com>
2026-03-22 22:28:50 +0530

    next day

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { useLiveQuery } from "dexie-react-hooks";
 import Signup from "./modules/Signup";
 import Todos from "./modules/Todos";
 import Signin from "./modules/Signin";
-import useAuth from "./hooks/useAuth";
 
 
 export default function App() {
@@ -20,15 +19,7 @@ export default function App() {
     status = unSyncTodos.length > 0 ? "unsynced" : "synced";
   }
 
-  const {auth,setAuth} = useAuth()
-  
-  // if(auth){
-  //   return(
-  //     <div>
-  //       {auth.email}
-  //     </div>
-  //   )
-  // }
+
    
   return (
     <Routes>
