2ca5f273349ecbddf8f50f5dd45c75b74edbfa87
Amanrathi07 <amanrathi4321@gmail.com>
2026-03-26 22:36:54 +0530

    added rateLimte

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,16 +4,25 @@ import cors from "cors"
 import authRoute from "./routes/auth.route";
 import cookieParser from "cookie-parser"
 import todoRouter from "./routes/todo.route";
+import { limiter } from "./middleware/ratelimte";
 
+dotenv.config()
 const app = express() ;
+app.set("trust proxy", 1);
 
-app.use(cookieParser()) 
-app.use(express.json())
 app.use(cors({
     origin: "http://localhost:5173", 
     credentials: true
-}))
-dotenv.config()
+}));
+
+app.use(express.json());
+
+app.use(cookieParser()) ;
+
+app.use(limiter);
+
+
+
 
 
 app.get("/helthCheck",(req,res)=>{
