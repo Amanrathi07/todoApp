ad6c7da15ec6efb9aff88c009f305a07278db909
Amanrathi07 <amanrathi4321@gmail.com>
2026-03-29 22:59:17 +0530

    singliton

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -3,8 +3,16 @@
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from './generated/prisma/client';
 
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-const adapter = new PrismaPg({connectionString:process.env.DATABASE_URL!})
+function CreatePrismaClient(){
+    const adapter = new PrismaPg({connectionString:process.env.DATABASE_URL!})
 
+    return new PrismaClient({adapter})
+}
 
-export const prismaClient = new PrismaClient({adapter})
+export const prismaClient = globalThis.prisma ??  CreatePrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
