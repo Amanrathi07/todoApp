79ea5bc3ef8c330883befc3f68babe64c5b6531a
Amanrathi07 <amanrathi4321@gmail.com>
2026-04-05 23:07:04 +0530

    ts error

--- frontend/src/functions/logout.ts
+++ frontend/src/functions/logout.ts
@@ -1,6 +1,11 @@
 import { db } from "../db";
 
-export  function handelLogout(setAuth:()=>void){
+type Prop = React.Dispatch<React.SetStateAction<{
+    name: string;
+    email: string;
+} | null>>
+
+export  function handelLogout(setAuth:Prop){
         localStorage.removeItem("todoAuth") ,
         
         document.cookie = "todoCookie" + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
