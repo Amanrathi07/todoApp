b576dcb6e6b51b097de6663e1d7d6adc44d109b3
Amanrathi07 <amanrathi4321@gmail.com>
2026-04-08 23:20:14 +0530

    add firebase forntend

---
+++ frontend/src/firebase/firebase.ts
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAEUQOBioZq9Pkuq1Z6gfYJN2z7UkuN1wg",
+  authDomain: "todoapp-be3d8.firebaseapp.com",
+  projectId: "todoapp-be3d8",
+  storageBucket: "todoapp-be3d8.firebasestorage.app",
+  messagingSenderId: "734026795781",
+  appId: "1:734026795781:web:e987114e38d6073898c56c"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
